b1d074bf065c3e0c14d293a7e13952d7c1685436
KevinHaltiner <kevin.haltiner@bluewin.ch>
2024-11-13 16:14:11 +0100

    Test

--- code/noten.php
+++ code/noten.php
@@ -63,12 +63,12 @@ function start()
                 system("clear");
                 break;
             case 'x':
-                echo "Programm wird beendet. Auf Wiedersehen!\n";
+                echo "Programm wird beendet. Auf Wiedersehen!\n"; //Beendet das Programm
                 break;
             default:
-                echo "Ungültige Auswahl! Bitte wählen Sie 1, 2, 3 oder x.\n";
-                readline("Drücken Sie Enter, um ins Menü zurückzukehren...");
                 system("clear");
+                echo "Ungültige Auswahl! Bitte wählen Sie 1, 2, 3 oder x.\n";
+                sleep(1);
                 break;
         }
     } while ($choice !== 'x');
@@ -79,24 +79,26 @@ function addGrade($file)
 {
     system("clear");
 
-    // Fächer zur Auswahl
+    // Fächerauswahl
     $subjects = ["Mathematik", "Englisch", "ABU", "Sport", "Modul 117", "Modul 231"];
     echo "\nWelches Fach möchten Sie auswählen?\n";
     foreach ($subjects as $index => $subject) {
         echo ($index + 1) . ". $subject\n";
     }
 
+    //Auswahl
     $subjectChoice = (int) readline("Ihre Auswahl (1 bis 6): ");
     if ($subjectChoice < 1 || $subjectChoice > 6) {
         echo "Ungültige Auswahl. Bitte versuchen Sie es erneut.\n";
         return;
     }
 
+    //Noten eintragen
     $selectedSubject = $subjects[$subjectChoice - 1];
     echo "\nSie haben $selectedSubject gewählt.\n";
     $newGrade = readline("Geben Sie die Note ein (zwischen 1 und 6): ");
 
-    // Validierung der Note
+    //Überprüft ob Note korrekt ist
     if (GradeCheck($newGrade)) {
         // Daten aus der Datei laden
         $data = file($file, FILE_IGNORE_NEW_LINES);
@@ -107,6 +109,7 @@ function addGrade($file)
             }
         }
 
+        //Bestätigung
         file_put_contents($file, implode(PHP_EOL, $data) . PHP_EOL);
         echo "Note erfolgreich hinzugefügt!\n";
     } else {
